8413936a78fe57e04956cdadb57ce22ff3551f10
AlfredoRago <alfredo.rago@gmail.com>
2019-10-02 11:26:34 +0200

    Select exons expressed in 2 out of 3 replicates

--- Scripts/CCRE_workflow/DE_gonads.R
+++ Scripts/CCRE_workflow/DE_gonads.R
@@ -2,6 +2,7 @@
 
 library(magrittr)
 library(stringr)
+library(plyr)
 
 ## Create output directories
 newdir<-file.path(getwd(), "Output/DE_gonads")
@@ -12,6 +13,7 @@ dir.create(graphdir)
 
 # Load eigenexon assignments
 eigenexons_assignments <- read.csv("./Output/exon_to_transcript_clustering_rankingonly_adjusted/eigenexons_assignments.csv")
+
 ## Load CCRE assignments
 CCREs <- read.csv(file = "./Output/CCREs/CCRE_assignments.csv")[,-1]
 names(CCREs) <- c("eigenexonID", "CCRE_ID", "Representative_Node")
@@ -30,8 +32,31 @@ gonad_expr_raw <-
 gonad_expr_raw<-gonad_expr_raw[which(duplicated(gonad_expr_raw$EXON)==F),]
 row.names(gonad_expr_raw) <- gonad_expr_raw$EXON
 gonad_expr_raw <- gonad_expr_raw[,-1]
-gonad_expr_raw$geneID<-str_extract(row.names(gonad_expr_raw), "^[^t]*") 
+# gonad_expr_raw$geneID<-str_extract(row.names(gonad_expr_raw), "^[^t]*") 
 # including the t postscript to disambiguate *b and non b gene names
+
+## Normalize exon expression
+# Select same threshold as main experiment
+threshold = 1.66
+# Remove exons that not expressed in at least 2 out of 3 samples
+biorep <- as.factor(str_extract(string = names(gonad_expr_raw), pattern = "^[^.]*"))
+t_gonad_expr_raw <- data.frame(biorep, t(gonad_expr_raw)) # remove subselection of exons after debugging
+l_gonad_expr_raw <- dlply(.data = t_gonad_expr_raw, .variables = .(biorep))
+
+expressed <- sapply(l_gonad_expr_raw, function(x){
+  apply(X = x, MARGIN = 2, FUN = function(y){
+    sum(y>threshold)>1
+  })
+})
+expressed <- expressed[-1,]
+row.names(expressed) <- row.names(gonad_expr_raw)
+# Select only exons expressed in at least one biological condition
+pass <- apply(X = expressed[-1,], MARGIN = 1, FUN = function(x){any(x==T)})
+pass <- names(pass)[which(pass==T)]
+# Subset exon dataset only to exons expressed in at least one biological condition
+gonad_expr_raw <- gonad_expr_raw[which(row.names(gonad_expr_raw)%in%pass),]
+# How many exons are left?
+nrow(gonad_expr_raw)
 
 
 ## Bin eigenexons
