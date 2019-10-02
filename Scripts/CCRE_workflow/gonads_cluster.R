d191a87c1afd98decf2f29ed300c0e9341770294
AlfredoRago <alfredo.rago@gmail.com>
2019-10-02 14:25:51 +0200

    Remove splicing nodes whose transcription node has no expression in the samples of interest

--- Scripts/CCRE_workflow/gonads_cluster.R
+++ Scripts/CCRE_workflow/gonads_cluster.R
@@ -1,6 +1,8 @@
 ## Cluster gonad expression by eigenexon, normalize and then cluster by CCRE
 
 library(FESTA)
+library(magrittr)
+library(stringr)
 
 ## Create output directories
 newdir<-file.path(getwd(), "Output/gonads_cluster")
@@ -11,6 +13,7 @@ dir.create(graphdir)
 # Load eigenexon assignments
 eigenexons_assignments <- read.csv("./Output/exon_to_transcript_clustering_rankingonly_adjusted/eigenexons_assignments.csv")
 names(eigenexons_assignments)[7] <- "transcriptID"
+eigenexons_assignments <- eigenexons_assignments[,-4]
 
 ## Load CCRE assignments
 CCREs <- read.csv(file = "./Output/CCREs/CCRE_assignments.csv")[,-1]
@@ -24,12 +27,27 @@ gonad_expr <- gonad_expr[which(row.names(gonad_expr)%in%eigenexons_assignments$e
 ## Filter only transcripts that are present in gonads/soma
 eigenexons_assignments <- eigenexons_assignments[which(eigenexons_assignments$exonID%in%row.names(gonad_expr)),]
 
+## Remove isoforms that have no main gene expression
+no_constitutive <- setdiff(
+  eigenexons_assignments[eigenexons_assignments$constitutive=='facultative', "geneID"],
+  eigenexons_assignments[eigenexons_assignments$constitutive=='constitutive', "geneID"]
+)
+
+no_constitutive_exons <- str_extract(row.names(gonad_expr), "^[^t]*") %>%
+  is_in(., no_constitutive) %>%
+  which(. == T) 
+gonad_expr <- gonad_expr[-no_constitutive_exons, ]
+
+eigenexons_assignments <- eigenexons_assignments[-which(eigenexons_assignments$geneID %in% no_constitutive),]
+
+
 ## Bin eigenexons
+debugonce(FESTA::AverageExons)
 gonad_expr_eigenexons <- AverageExons(
   data = gonad_expr, 
   spliceID = eigenexons_assignments,
-  splicingRatios = F,
-  NAcorrection = F
+  splicingRatios = T,
+  NAcorrection = T
 )
 
 ## Bin CCREs
