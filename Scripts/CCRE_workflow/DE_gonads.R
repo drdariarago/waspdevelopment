35e4133e4f1fb9836a67524ca7acbe675f8b0d6a
AlfredoRago <alfredo.rago@gmail.com>
2019-10-02 11:01:34 +0200

    Import expression for gonad and respective whole-organism stages

---
+++ Scripts/CCRE_workflow/DE_gonads.R
@@ -0,0 +1,56 @@
+### Gonad analyses
+
+library(magrittr)
+library(stringr)
+
+## Create output directories
+newdir<-file.path(getwd(), "Output/DE_gonads")
+dir.create(newdir)
+graphdir<-file.path(getwd(), "Graphics/DE_gonads")
+dir.create(graphdir)
+
+
+# Load eigenexon assignments
+eigenexons_assignments <- read.csv("./Output/exon_to_transcript_clustering_rankingonly_adjusted/eigenexons_assignments.csv")
+## Load CCRE assignments
+CCREs <- read.csv(file = "./Output/CCREs/CCRE_assignments.csv")[,-1]
+names(CCREs) <- c("eigenexonID", "CCRE_ID", "Representative_Node")
+
+
+## Import expression for gonads and respective whole-animal stages
+# Import exon-level expression
+nasonia_devtesova.exon.r99 <- read.delim("./Nasonia Score files/2015_05_19_Justin/nasonia_devtesova.exon.r99.score2")
+# Select only gonads and respective whole-animal stages (male pupae, female adults)
+gonad_expr_raw <- 
+  c("EXON|testes.[1-3]|ovaries.[1-3]|adult_female.[1-3]|pupyel_male.[1-3]") %>% 
+  grep(pattern = ., names(nasonia_devtesova.exon.r99), value = T) %>%
+  nasonia_devtesova.exon.r99[,.]
+
+# remove duplicated exons add gene IDs
+gonad_expr_raw<-gonad_expr_raw[which(duplicated(gonad_expr_raw$EXON)==F),]
+row.names(gonad_expr_raw) <- gonad_expr_raw$EXON
+gonad_expr_raw <- gonad_expr_raw[,-1]
+gonad_expr_raw$geneID<-str_extract(row.names(gonad_expr_raw), "^[^t]*") 
+# including the t postscript to disambiguate *b and non b gene names
+
+
+## Bin eigenexons
+
+
+## Bin according to CCREs
+
+
+
+## Perform DE between ovaries and adult females
+
+
+## Perform DE between testes and male pupae
+
+
+## Categorize in: male gonads, female gonads, general gonads
+
+
+## Intersect with other DE data from development
+
+
+## Categorize proportion of Development DE from gonad expression
