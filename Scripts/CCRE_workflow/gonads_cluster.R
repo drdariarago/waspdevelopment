54c76f4ec48a0b169bbb5c1cf23531b810cf5c07
AlfredoRago <alfredo.rago@gmail.com>
2019-10-02 16:47:07 +0200

    Rescale expression and peform QC
    Also includes changes to the creation of the CCRE index

--- Scripts/CCRE_workflow/gonads_cluster.R
+++ Scripts/CCRE_workflow/gonads_cluster.R
@@ -49,28 +49,54 @@ gonad_expr_eigenexons <- AverageExons(
   splicingRatios = T,
   NAcorrection = T
 )
-write.csv(gonad_expr_eigenexons, file=file.path(newdir,"gonad_expr_eigenexons.csv"), row.names=F)
+
+rownames(gonad_expr_eigenexons) <- gonad_expr_eigenexons$transcriptID
 
 ## Normalize dataset
+# Rescale costitutive nodes to 0-1 space by dividing by largest expression value
+max_expr <- 
+  gonad_expr_eigenexons[,grep(pattern = "[0-3]$", x = colnames(gonad_expr_eigenexons))] %>%
+  c(.) %>% unlist(.) %>%
+  max(.)
 
+gonad_expr_eigenexons_rescaled_con <- 
+  gonad_expr_eigenexons[grep(pattern = "_con$", x = gonad_expr_eigenexons$transcriptID), -(1:2)] %>%
+  apply(., c(1,2), function(x){ x / max_expr }) 
+
+gonad_expr_eigenexons_rescaled <- 
+  gonad_expr_eigenexons[grep(pattern = "_fac$", x = gonad_expr_eigenexons$transcriptID), -(1:2)] %>%
+  rbind(gonad_expr_eigenexons_rescaled_con, .)
+
+gonad_expr_eigenexons_rescaled <- 
+  gonad_expr_eigenexons_rescaled[sort(row.names(gonad_expr_eigenexons_rescaled)),]
+
+write.csv(gonad_expr_eigenexons_rescaled, file=file.path(newdir,"gonad_expr_eigenexons.csv"), row.names=F)
+
+# QC filtering of genes with low variance or low expression
+# Check for zero variance nodes and nodes with too many missing values
+goodCols<-goodSamplesGenes(
+  datExpr = transposeBigData(gonad_expr_eigenexons_rescaled),
+  minNSamples = 3, minFraction = 1/10)
+lapply(goodCols, function(x) {table(x)})
+lapply(goodCols, function(x) {prop.table(table(x))})
 
 ## Bin CCREs
 
 # Subset only CCRE transcripts
-eigenexon_CCRE <- gonad_expr_eigenexons[which(gonad_expr_eigenexons$transcriptID %in% CCREs$eigenexonID),]
+eigenexon_CCRE <- gonad_expr_eigenexons_rescaled[which(row.names(gonad_expr_eigenexons_rescaled) %in% CCREs$eigenexonID),]
 
 # Create match table between row IDs and groups
 CCRE_lookup <- merge(
-  eigenexon_CCRE[,"transcriptID", drop=F], CCREs, 
-  by.x = 'transcriptID', by.y = 'eigenexonID',
-  all.x = T, all.y = F)
+  eigenexon_CCRE[,1 , drop=F], CCREs, 
+  by.x = 'row.names', by.y = 'eigenexonID',
+  all.x = T, all.y = F)[,-2]
 
 # Create expression matrix for eigenexons that need to be converted to CCREs
 row.names(eigenexon_CCRE) <- eigenexon_CCRE$transcriptID
 eigenexon_CCRE_expression <- eigenexon_CCRE[,grep(pattern = "[0-3]$", x = colnames(eigenexon_CCRE))]
 eigenexon_CCRE_expression <- as.matrix(as.numeric(eigenexon_CCRE_expression))
-# Collapse CCREs
 
+# Collapse CCREs
 CCRE_list <- collapseRows(
   datET = eigenexon_CCRE, 
   rowGroup = CCRE_lookup$CCRE_ID, 
